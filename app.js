779bc4e0c3a5a55475dcadc05b5e8623721ea641
Queen King <king@Queens-iMac.local>
2021-03-01 15:46:38 +0200

    mongodb

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 // console.log('App!')
 const express = require('express')
 const config = require('config')
+const mongoose = require('mongoose')
 
 const app = express()
 
 const PORT = config.get('port') || 5000
 
-app.listen(PORT, () => console.log(`app has been started on port...${PORT}`))
+async function start() {
+  try {
+    await mongoose.connect(config.get('mongoUri'), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    })
+    //run after connect to dbserver ru
+    app.listen(PORT, () => console.log(`app has been started on port...${PORT}`))
+
+  } catch (e) {
+    console.log('Server Error', e.message)
+    process.exit(1)
+  }
+}
+start()
