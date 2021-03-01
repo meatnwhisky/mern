1f9155b0190a60684fd578bb63bf769586ecdd0b
Queen King <king@Queens-iMac.local>
2021-03-01 12:40:44 +0200

    env-config-set

---
+++ app.js
@@ -0,0 +1,9 @@
+// console.log('App!')
+const express = require('express')
+const config = require('config')
+
+const app = express()
+
+const PORT = config.get('port') || 5000
+
+app.listen(PORT, () => console.log(`app has been started on port...${PORT}`))
